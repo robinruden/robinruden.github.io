1b6ed89c289074d9d2867273a62cebe8ae598590
Robin Rudén <robinruden@gmail.com>
2025-08-13 20:54:26 +0200

    Rexico City icon is added to the folder

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -3,13 +3,25 @@
 import "./Folder.css"
 import { useState } from "react"
 
-function Folder() {
+const folderIcons = [
+  { id: "project1", name: "Rexico City", icon: "/img/rexico-logga.ico" },
+  { id: "project2", name: "Project 2", icon: "/img/project2.ico" },
+  // Add more icons as needed
+]
 
+function Folder({ onIconClick }) {
   return (
-    <div className="folder">
-    
-     <div></div>
-
+    <div className="folder-content">
+      {folderIcons.map(icon => (
+        <div
+          key={icon.id}
+          className="folder-icon"
+          onClick={() => onIconClick && onIconClick(icon.id)}
+        >
+          <img src={icon.icon} alt={icon.name} className="icon-image" />
+          <div className="icon-text">{icon.name}</div>
+        </div>
+      ))}
     </div>
   )
 }
