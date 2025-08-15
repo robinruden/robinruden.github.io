c432fff22130e24bc16dd8e831f0556e5cac261d
Robin Rudén <robinruden@gmail.com>
2025-08-15 12:20:10 +0200

    updated styling

--- src/components/RexicoCity.jsx
+++ src/components/RexicoCity.jsx
@@ -9,17 +9,18 @@ function RexicoCity() {
   return (
     <div className="rexico-city">
        <div className="tech-description">
-        <h2>Welcome to Rexico City. Feel free to explore our brand new dinosaur park. Your journey starts here!</h2>
-        <ul>
-          <li><strong>Vue</strong> - Component-based UI framework</li>
-          <li><strong>Vite</strong> - Fast build tool and development server</li>
-        </ul>
-      </div>
+        <h2>Welcome to Rexico City. Your journey starts here!</h2>
+        
+          
+        
+      <div class="rexico-img">
       <a href="https://rexicocity.netlify.app" target="_blank" rel="noopener noreferrer">
       <img src="/img/rexico.png" alt="" />
       </a>
-
-      
+      </div>
+      <li><strong>Vue</strong> - Component-based UI framework</li>
+      <li><strong>Vite</strong> - Fast build tool and development server</li>
+      </div>
     </div>
   )
 }
